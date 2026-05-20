093d4decdd86cd2112cbfa131cb0357ab94b32b4
Babisca Paul <babisca.paul1@gmail.com>
2026-05-20 14:12:41 +0300

    Implement runs endpoints

--- api/app/main.py
+++ api/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.db import db_lifespan
-from app.routers import health
+from app.routers import health, runs
 from app.settings import get_settings
 
 settings = get_settings()
@@ -22,3 +22,4 @@ app.add_middleware(
 )
 
 app.include_router(health.router)
+app.include_router(runs.router)
